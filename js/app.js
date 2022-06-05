705ecf788283b3366c3b13001a1e387ccec3bca6
Nhan0504 <ttn0504ltt@gmail.com>
2022-06-05 10:16:12 -0400

    Name classes for check, uncheck, and line through

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
+
+// Classes name
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
