d32e8a2c93510b34a20b4d8db4b95c158a48fe56
Nhan0504 <ttn0504ltt@gmail.com>
2022-06-05 10:12:13 -0400

    Select element in the html file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-// CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
