0bd59cfd56a4d490cbe945a56f0a0ea42660975c
Nhan0504 <ttn0504ltt@gmail.com>
2022-06-05 10:33:02 -0400

    Function to add a to do

--- js/app.js
+++ js/app.js
@@ -12,3 +12,17 @@ const LINE_THROUGH = "lineThrough";
 const options = {weekday : "long", month: "short", day: "numeric"};
 const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+// Add to do
+function addToDo(toDo) {
+    const item = `
+                <li class="item">
+                    <i class="fa fa-circle-thin co" job="complete" id = 0></i>
+                        <p class="text">${toDo}</p>
+                        <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                </li>
+                `
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, item);
+}
+addToDo("hello");
